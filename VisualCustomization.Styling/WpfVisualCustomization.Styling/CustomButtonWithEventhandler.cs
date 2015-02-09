4e150258da4f2f55781034e4ec1584cd3581a1aa
sergedesmedt <sergedesmedt@yahoo.com>
2015-02-09 21:59:57 +0100

    initial commit

---
+++ VisualCustomization.Styling/WpfVisualCustomization.Styling/CustomButtonWithEventhandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace VisualCustomization
+{
+    public class CustomButtonWithEventhandler : Button
+    {
+        public CustomButtonWithEventhandler()
+        {
+            this.Click += CustomButtonWithEventhandler_Click;
+        }
+
+        void CustomButtonWithEventhandler_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            MessageBox.Show("I'm the constructor attached eventhandler, and I'm still invoked"); ;
+        }
+    }
+}
---
+++ VisualCustomization.Styling/WpfVisualCustomization.Styling/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace VisualCustomization
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            AnotherWindow anotherWindow = new AnotherWindow();
+            anotherWindow.Show();
+        }
+
+        private void CustomButtonWithEventhandler_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("I'm the window attached eventhandler, and I'm still invoked");
+        }
+
+        private void BrownButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked the Brown Button!");
+        }
+
+        private void RedButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked the Red Button!");
+        }
+
+        private void ButtonInContainer_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked the Button in the StackPanel!");
+        }
+
+        private void WowButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("You clicked the Button inside theh component (or not?)!");
+        }
+    }
+}
