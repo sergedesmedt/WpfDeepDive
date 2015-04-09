4c821931abf9b1b7611e54354a6f90a80c3eb9c5
sergedesmedt <sergedesmedt@yahoo.com>
2015-04-09 13:19:59 +0200

    Started Resource samples

---
+++ General.Resources/Resources/MyCustomClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Resources
+{
+    public class MyCustomClass
+    {
+        public string StringProperty { get; set; }
+        public int IntegerPoperty { get; set; }
+
+        public override string ToString()
+        {
+            return "This MyCustomClass instance has a value for StringProperty of [" + StringProperty + "] and a value of IntegerProperty of [" + IntegerPoperty + "]";
+        }
+    }
+}
